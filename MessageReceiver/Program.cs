9158b9123f79f9642fddc6fd0fbd9530200ecc54
Aydar Shamsutdinov <aydarrrio@gmail.com>
2017-02-02 22:48:48 +1100

    sender

---
+++ MessageReceiver/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace MessageReceiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory {HostName = "localhost"};
+            using (var conn = factory.CreateConnection())
+            {
+                using (var chann = conn.CreateModel())
+                {
+                    chann.QueueDeclare("hello", false, false, false);
+                    var consumer = new EventingBasicConsumer(chann);
+                    consumer.Received += (sender, eventArgs) =>
+                    {
+                        var body = eventArgs.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine("* Received: {0}", message);
+                    };
+                    chann.BasicConsume("hello", true, consumer);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
---
+++ MessageSender/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace MessageSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using (var conn = factory.CreateConnection())
+            {
+                using (var chann = conn.CreateModel())
+                {
+                    chann.QueueDeclare("hello", false, false, false);
+                    Console.WriteLine("Type [exit] to exit or message to send");
+                    var run = true;
+                    while (run)
+                    {
+                        var line = Console.ReadLine();
+                        if (line == "exit")
+                        {
+                            run = false;
+                        }
+                        else
+                        {
+                            var body = Encoding.UTF8.GetBytes(line);
+                            chann.BasicPublish("", "hello", null, body);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
